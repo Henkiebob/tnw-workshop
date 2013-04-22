06a760c0291ce9fe23227bf2152504207bf65e29
Vincent Bremer <vbremer89@gmail.com>
2013-04-22 12:22:02 +0200

    Added the scrollable functions

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,6 +1,26 @@
-$(document).ready(function(){
+$(document).ready(function() {
+	// Set the focus to the textfield.
+	$('.textfield').focus();
+
+	// Check if the form has been submitted
 	$('form').submit(function(e){
+
+		// Prevent the form from being submitted
 		e.preventDefault();
-		$('.message').append('<p>aa</p>');
+
+		// Get the text from the textfield
+		var text = $('.textfield').val();
+
+		// Empty the textfield
+		$('.textfield').val('');
+
+		// Append the inserted text to the div
+		$('.messages > div').append('<p>'+text+'</p>');
+
+		// Scroll the window to the bottom
+		$('.messages').scrollToBottom(100);
+
+		// Set the focus to the textfield.
+		$('.textfield').focus();
 	});
 });
